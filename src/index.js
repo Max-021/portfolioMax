7bd5f8b1699b8a09a80e90c254ed3e1e2851fa52
Maximiliano Frega <maxifregawebdev@gmail.com>
2024-02-08 21:13:55 -0300

    cambios en hashrouter

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <App />
   </HashRouter>
 );
